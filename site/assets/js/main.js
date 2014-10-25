64cb0e6fc129b9ecd06337f745cc6aca6cbbbf39
Rob Righer <rrighter@salesforce.com>
2014-10-25 16:38:16 -0400

    fix typos and other add pardot tracking

--- site/assets/js/main.js
+++ site/assets/js/main.js
@@ -1,11 +1,40 @@
 ;(function(){
 
-			// Menu settings
-			$('#menuToggle, .menu-close').on('click', function(){
-				$('#menuToggle').toggleClass('active');
-				$('body').toggleClass('body-push-toleft');
-				$('#theMenu').toggleClass('menu-open');
-			});
+	// Menu settings
+	$('#menuToggle, .menu-close').on('click', function(){
+		$('#menuToggle').toggleClass('active');
+		$('body').toggleClass('body-push-toleft');
+		$('#theMenu').toggleClass('menu-open');
+	});
+
+	function getSearchParameters() {
+		var prmstr = window.location.search.substr(1);
+		return prmstr != null && prmstr != "" ? transformToAssocArray(prmstr) : {};
+	}
+
+	function transformToAssocArray( prmstr ) {
+		var params = {};
+		var prmarr = prmstr.split("&");
+		for ( var i = 0; i < prmarr.length; i++) {
+		    var tmparr = prmarr[i].split("=");
+		    params[tmparr[0]] = tmparr[1];
+		}
+		return params;
+	}
+
+	var params = getSearchParameters();
+	if(params.hasOwnProperty('errors')){
+		if(params.errors.substr(0,4) === 'true'){
+			$('#exampleInputEmail').css('background-color','#B63D3D');
+			$("html, body").animate({ scrollTop: $(document).height() }, "fast");
+		}
+		else{
+			$('#confirmmessage').text('Thank you for contacting us, we are looking forward to meeting you.');
+			$('#confirmmessage').slideDown('fast');
+		}
+	}
+	console.log('Params are');
+	console.log(params);
 
 
 })(jQuery)
